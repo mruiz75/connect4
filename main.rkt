5e7730e7c02566876ad3fc7f5cd0a71784faa64c
mruiz75 <39355128+mruiz75@users.noreply.github.com>
2020-03-12 12:15:01 -0600

    fix

--- main.rkt
+++ main.rkt
@@ -3,22 +3,37 @@
 (require racket/gui)
 (require (lib "graphics.ss" "graphics")) (open-graphics)
 
-(define window (open-viewport "window" 600 700))
+(define window (open-viewport "window" 700 600))
 
-(define window2 (open-pixmap "invisibleWindow" 600 700))
-((draw-solid-rectangle window2) (make-posn 0 0) 600 700 "white")
+(define window2 (open-pixmap "invisibleWindow" 700 600))
+((draw-solid-rectangle window2) (make-posn 0 0) 700 600 "white")
 
 (define gameBoard
   (for/vector ([i (in-range 6)])
     (for/vector ([j (in-range 7)])
-      random 0)))
+      (random 1))))
 
-(define (setGameBoardCoordinate row column value)
+(define (setGameBoardInt row column value)
   (vector-set! (vector-ref gameBoard row) column value))
+
+(define (getGameBoardInt row column)
+  (vector-ref (vector-ref gameBoard row) column))
+
+(define (showBoard gameBoard)
+  (for/vector ([i (in-range 6)])
+    (for/vector ([j (in-range 7)])
+      (cond
+        [(= (getGameBoardInt i j) 0) ((draw-pixmap window2) "/Users/Manuel/Documents/TEC/2020/IA/connect4/tiles/whiteTile.png" (make-posn (* 100 j) (* 100 i)))]
+        [(= (getGameBoardInt i j) 1) ((draw-pixmap window2) "/Users/Manuel/Documents/TEC/2020/IA/connect4/tiles/redTile.png" (make-posn (* 100 j) (* 100 i)))]
+        [(= (getGameBoardInt i j) 2) ((draw-pixmap window2) "/Users/Manuel/Documents/TEC/2020/IA/connect4/tiles/blueTile.png" (make-posn (* 100 j) (* 100 i)))])
+        ))
+  (copy-viewport window2 window))
+
 
 (define (startConnect4 option)
   (cond [(equal? option 0)
-         ()]
+         (showBoard gameBoard)
+         ]
         [(equal? option 1)
          (close-graphics)
          (exit)]
@@ -32,20 +47,20 @@
 (define winFrame (new frame%
                    [label "Conecta 4"]
                    [width 700]
-                   [600]))
+                   [height 600]))
 
 (define loseFrame (new frame%
                    [label "Conecta 4"]
                    [width 700]
-                   [600]))
+                   [height 600]))
 
 (define msg (new message% [parent frame]
                           [label "Bienvenido a Conecta 4. Selecciona una opción:"]))
 
-(define winMsg (new message% [parent endFrame]
+(define winMsg (new message% [parent winFrame]
                           [label "Felicidades, has ganado el juego!!"]))
 
-(define loseMsg (new message% [parent endFrame]
+(define loseMsg (new message% [parent loseFrame]
                           [label "Ha ganado la computadora. Vuelve a intentarlo cuando quieras!"]))
 
 (new button% [parent frame]
@@ -60,10 +75,10 @@
                          (send frame show #f)
                          (startConnect4 1))])
 
-(new button% [parent frame]
+(new button% [parent loseFrame]
              [label "Volver al Menu Principal"]
              [callback (lambda (button event)
-                         (send endFrame show #f)
+                         (send loseFrame show #f)
                          (startConnect4 1))])
 
 (send frame show #t)
